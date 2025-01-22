679d1a841bddb740bb9054da9c6f42bf1fecc43c
sudarshand <sudarshan.damodharan@gmail.com>
2025-01-21 17:56:34 -0700

    make plots on same figure

--- Homeworks/HW1/polyplot.py
+++ Homeworks/HW1/polyplot.py
@@ -7,21 +7,10 @@ coeffs = [1, -18, 144, -672, 2016, -4032, 5376, -4608, 2304, -512]
 polyval_coeffs = np.polyval(coeffs, x_vals)
 polyval_fact = (x_vals - 2) ** 9
 
-#Coefficent Plot
 plt.figure(figsize=(8, 6))
-plt.plot(x_vals, polyval_coeffs, 'b-')
-plt.title("Plot of $p(x)$ using Expanded Coefficients")
-plt.xlabel("x")
-plt.ylabel("$p(x)$")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-#Factorized Plot
-plt.figure(figsize=(8, 6))
-plt.plot(x_vals, polyval_fact, 'r-')
-plt.title("Factorized Evaluation $(x-2)^9$")
+plt.plot(x_vals, polyval_coeffs, 'b-', label="Expanded $p(x)$")
+plt.plot(x_vals, polyval_fact, 'r-', label="Factorized $(x-2)^9$")
+plt.title("Comparison of Expanded and Factorized Polynomial")
 plt.xlabel("x")
 plt.ylabel("$p(x)$")
 plt.grid(True)
